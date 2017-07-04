de0a7da29f3e9bbb986a33904ab946895377e696
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-04 22:00:24 +0100

    text edit added to app

--- app/src/main/java/com/example/user/wordcountjava/WordCountActivity.java
+++ app/src/main/java/com/example/user/wordcountjava/WordCountActivity.java
@@ -2,6 +2,7 @@ package com.example.user.wordcountjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WordCountActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class WordCountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_count);
+        Log.d(getClass().toString(), "onCreate called");
     }
 }
 
